5ca9d085908df9df31d48d428aa1fbf7b61b3e34
Master Silver <silver@mindgrep.com>
2016-05-02 14:05:48 +0530

    added assignment, conditional assignment, boolean expression

--- test.js
+++ test.js
@@ -2,39 +2,111 @@ var EpicSearch = require('./index')
 var config = require('./config')
 var es = new EpicSearch(config)
 
-
-
-/**es.crudUpdate({
-    type: 'event',
-    _id: 'AVHDEPUuQpxtrIE1g6xi',
-    update: {
-      set: {
-        title_english: 'vab'
-      }
-    }
-  })**/
-
-  es.queryParser.parse([
-    'get test *id as ab',
-    'get test *ab._id',
-    'search test where {x: "*ab._source.x"} as xx'
-  ], {id : 1})
-  .then(function(res) {
-    console.log(res)
-  })
-  .catch(console.log)
-
-  
 es.update.deep({
     _index: 'test',
-    _type: 'entity',
+    _type: 'test',
     _id: '1',
     context: {
-      "fields": ['a']
+      fields: ['a']
     },
-    lang: "english"
+    lang: 'english'
   })
   .then(function(res) {
     console.log(2, 'get', res)
   })
   .catch(console.log)
+/**es.queryParser.parse([
+  'async each *ids as ida',[
+    'get test *ida as idaTest',
+    'async each *ids as id',[
+      'get test *id as x',
+      'addToSet *idaTest._id in *x at _source.y'
+    ]
+  ]
+], {ids: [1, 2]})
+.then(console.log)
+.catch((e) => {
+  console.log(e, e.stack)
+})**/
+
+/**'search person where {"name.suggest": *text} as persons. Retrieve field speakers',
+'async each persons.hits.hits as person',[
+  'add every *person.speakers to *speakerIds'
+]
+'search speaker where {_id is one of *speakerIds} as speakers. Join with person(retrieve only name), primaryLanguages (retrieve only name), secondaryLanguages (retrieve only name)',
+
+
+'get videos where speaker.person.name = Dalai and event.venue.name = tipa'
+'search speakers where person.name = *text'
+
+'search video where {event.venue.name is *X AND speakers.person.name is *Y}'
+{
+  type: video,
+  fields: {
+    processingStatus: 'edit'
+  },
+  relationships: {
+    event: {
+      relationships: {
+        venue: {
+          fields: {
+            name: X
+          }
+        }
+      },
+      fields: {
+        startingDate: {
+          gt: 44,
+          lt: 55
+        }
+      }
+    },
+    speakers: {
+      relationships: {
+        person: {
+          fields: {
+            name: Y
+          }
+        }
+      }
+    }
+  }
+}
+**/
+
+
+
+
+const assignments = ['x is 2', 'x is 4 if *x is 2 ? Else 1', 'x is 1 if *x is 4 ? Else 2' ]
+var isEqual = ['2 is 4?', '2 is 2?', '*x is 2?', '*x is 2 strict?']
+const asyncEachThenGet = [
+  'async each *arr as i',
+    [
+      'get test *i',
+    ]
+]
+var testInstructions = [
+  'get event *content.eventId',
+  'search content-to-audio-channel where {contentId: "*content._id"} as cac. Retrieve fields audioChannelId',
+  'async each cac.hits.hits as contentToAudioChannel',
+    [
+      'get audiochannel *contentToAudioChannel._source.audiochannelId',
+
+      'speakerType is speaker if *audioChannel.translationType is empty? Else is translator',
+
+      'speakerFields are {personId: "*audiochannel.speakerId", primaryLanguages: "*audiochannel.languageId"}',
+      'speakerFields.translationType is *audioChannel.translationType if *audioChannel.translationType is not empty',
+
+      'search first *speakerType with *speakerFields as *speakerType. Create if needed',
+
+      'add *speaker._id to *event.speakers if not there already',
+      'add *audioChannel.languageId to *event.primaryLanguages if not there already',
+    ],
+  'index event'
+]
+
+es.queryParser.parse(asyncEachThenGet, {x: 2, arr: [1, 2]})
+.then(function(res) {
+  console.log(JSON.stringify(res))
+})
+.catch(console.log)
