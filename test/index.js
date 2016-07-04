3d97bcc159c4739e172fcda2dde56710fd7bf908
Ashutosh Tripathi <ashu17706@gmail.com>
2016-07-04 10:59:47 +0530

    before() for collect tests

--- test/index.js
+++ test/index.js
@@ -1,3 +1,4 @@
+'use strict';
 require('./common.js');
 
 describe('testing epicsearch', function() {
@@ -5,21 +6,36 @@ describe('testing epicsearch', function() {
   this.timeout(15000);
 
   describe('collect API', () => {
-    require('./collect/aggregator.js');
+    before(() => {
+      let objects = [
+        {index: 'tests', type: 'test', id: 1, body: {field : "Test 1"}},
+        {index: 'tests', type: 'test', id: 2, body: {field : "Test 2"}},
+      ]
+
+      objects.forEach((object) => {
+        es.create(object)
+      })
+
+      console.log("\tObjects have been created for testing")
+    })
+
     require('./collect/get/index');
     require('./collect/get/mget');
+    require('./collect/index/index');
     require('./collect/search/index');
     require('./collect/search/multi_search');
+    //bulk delete opration for created test documents
     require('./collect/bulk/index');
-    require('./collect/index/index');
+    require('./collect/aggregator.js');
   });
 
   describe('DSL API', () => {
     require('./dslEngine/booleanExpression')
     require('./dslEngine/search')
-  });
+    require('./dslEngine/async')
+    });
 
-  describe('deep API', () => {
+    describe('deep API', () => {
 
-  });
+   });
 });
