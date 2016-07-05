a0a47675e73a75bae8ec78eb7cb97d3d65febb3b
Master Silver <silver@mindgrep.com>
2016-07-05 12:41:22 +0530

    deep/utils: added getEntitiesAtPath/AtEdge && WIP calculateUnionInGraph

--- test.js
+++ test.js
@@ -43,7 +43,7 @@ var es = new EpicSearch('/home/master/work/code/epicsearch/newConfig')
 //'search speakers where person.name = *text'
 //const assignments = ['x is 2', 'x is 4 if *x is 2 ? Else 1', 'x is 1 if *x is 42 ? Else is 2']
 const assignments = ['k is {d: "*x", *x: 4} if *x is empty. Else 22' ]
-
+const get = ['get event 1']
 const isEqual = ['2 is 4?', '2 is 2?', '*x is 2?', '*x is 2 strict?']
 
 const isEmpty = ['*x is not empty']
@@ -66,7 +66,7 @@ const asyncEachThenGet = [
     ]
 ]
 
-const index = ['index *y as type a', 'index *m']
+const index = ['index *y as type a', 'index *m', 'index *session']
 
 var testInstructions = [ //Get and search retrieve entity object(s) from in memory cache which in turn is flled from ES as each respective query happens for first time. These are mutable objects. The idea is to let them go through a process of multiple mutations in memory during the migration process. Once the old tables are processed, (only) the dirty entities in cache are flushed to ES.
   'get event *content.eventId',
@@ -89,23 +89,31 @@ var testInstructions = [ //Get and search retrieve entity object(s) from in memo
     ],
 ]
 
-const ctx = {x: [7], y: {arr: [1,2]}, arr: [1, 2], a: {_type: 'a', _id: '12'}, b: {_type: 'b', _id: '1'}, m: {_type: 'a', _source: {}, _id: '2'}}
-es.dsl.execute(link, ctx)
+const ctx = {x: [7], y: {arr: [1,2]}, arr: [1, 2], a: {_type: 'a', _id: '12'}, b: {_type: 'b', _id: '1'}, m: {_type: 'a', _source: {}, _id: '2'}, session: {_id: 1, _type: 'session', _source: {event: {_id: 1}, video: [{_id: 1}]}}}
+es.dsl.execute(index, ctx)
 .then(function(res) {
-  console.log(JSON.stringify(res), ctx.y)
+  console.log(JSON.stringify(res), ctx.test)
 })
 .catch(console.log)
 
 
 /***
  *THINGS TO DO
-Add fields and join in search DSL (low prio)
-Test update with copyTo, unionIn across deep paths
-Support for multi depth copy to declarations
+DONE Change from storing strings of foreign keys to objects with _id and own:true (of added directly to this entity, and is not union from another place)
+Support for multi depth join at storage level (denormalization)
+Support for multi lingual field in field def
+fix function results for lib/deep
+Support for multi depth unionIn with name of field included 
+
+
+
+Test update with joined data, unionIn across deep paths
+Add cache to dsl
+Generate ES index mappings though config (nested for relationships)
 Support for non schema type in queries
 Define web.search and web.read contexts in TOML
 db migration: create schema for DL
-fix function results for lib/deep
+Add fields and join in search DSL (low prio)
 
 TEST UPDATE
 PASS - If no field has changed in source entity, do not flush it
@@ -114,10 +122,9 @@ TEST UPDATE - Union in
 * Test copy to relationship across one level of depth, two levels of depth, then 4 levels of depth (induction principal)
 * PASS - Adding a new field to an array should update copyTo relationship
 * PASS - Setting a field should update copyTo relationship
-* Removing a field should unset it from copyTo relationships
 
-TEST UPDATE - Copy To
-* Test copy to relationship across two levels of depth, then 4 levels of depth
+TEST UPDATE - Joins
+* Test joined data management across two levels of depth, then 4 levels of depth
 * * Working - AddToSet, Set, unset, push, pull
 
 TEST UPDTAE - Union in
